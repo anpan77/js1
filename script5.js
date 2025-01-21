6ea0dbefb07d471450a10e0c59eb6222dc35e960
anpan77 <anpan715@gmail.com>
2025-01-21 10:10:05 +0900

    イベント発火基礎編

--- script5.js
+++ script5.js
@@ -22,3 +22,14 @@ lists.addEventListener('mouseover',function(){
 lists.addEventListener('mouseout',function(){
     lists.removeAttribute("style","buckground-color:red;")
 })
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down")
+pullDownButton.addEventListener("click",function(){
+    if(pullDownParents.getAttribute("style")=="display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示")
+    }else{
+        pullDownParents.setAttribute("style","display:block;")
+        console.log("表示")
+    }
+})
